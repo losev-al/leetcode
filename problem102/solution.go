b09ca3e858ba70e8a58fe021c748020776a619c2
Aleksey Losev <losev.aleksey3@wildberries.ru>
2020-10-25 07:30:41 +0300

    102

---
+++ problem102/solution.go
@@ -0,0 +1,32 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type queueItem struct {
+	Node  *TreeNode
+	Layer int
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	queue := make([]queueItem, 0)
+	queue = append(queue, queueItem{root, 0})
+	result := make([][]int, 0)
+	for len(queue) > 0 {
+		current := queue[0].Node
+		layer := queue[0].Layer
+		queue = queue[1:]
+		if current != nil {
+			if len(result) == layer {
+				result = append(result, make([]int, 0))
+			}
+			result[layer] = append(result[layer], current.Val)
+			queue = append(queue, queueItem{current.Left, layer + 1})
+			queue = append(queue, queueItem{current.Right, layer + 1})
+		}
+	}
+	return result
+}
